b5cc4fc89af8a9958d6727510b5f4f5c2fcbd025
Eichi2514 <picon1317@gmail.com>
2024-08-05 15:09:07 +0900

    Commit-test

--- src/main/java/com/KoreaIT/java/jsp_AM/HomeMainServlet.java
+++ src/main/java/com/KoreaIT/java/jsp_AM/HomeMainServlet.java
@@ -12,8 +12,6 @@ import java.io.IOException;
 
 @WebServlet("/home/main")
 public class HomeMainServlet extends HttpServlet {
-	private static final long serialVersionUID = 1L;
-       
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		response.getWriter().append("Hello World123").append(request.getContextPath());
 	}
---
+++ src/main/java/com/KoreaIT/java/jsp_AM/PrintDanServlet.java
@@ -0,0 +1,33 @@
+package com.KoreaIT.java.jsp_AM;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+
+
+
+@WebServlet("/printDan")
+public class PrintDanServlet extends HttpServlet {
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) 
+			throws ServletException, IOException {
+
+		response.setContentType("text/html;charset=UTF-8");
+        // Servlet 변수를 str1에 저장
+		String str1 = request.getParameter("dan");
+		
+        // str1을 형변환해서 dan에 저장
+		int dan = Integer.parseInt(str1);
+		
+		response.getWriter().append("=="+dan+"단==<br>");
+		
+		for(int i = 1; i <= 9; i++) {
+			response.getWriter().append(String.format("%d * %d = %d<br>", dan, i, dan*i));
+		}
+		
+	}
+}
